1d70ea782345f862f9ba578813c7135d4a572bd0
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-05-17 21:55:37 -0500

    UWP init

--- PixaImagenes.UWP/App.xaml.cs
+++ PixaImagenes.UWP/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FFImageLoading.Forms.WinUWP;
 
 namespace PixaImagenes.UWP
 {
@@ -59,6 +60,7 @@ namespace PixaImagenes.UWP
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
                 Xamarin.Forms.Forms.Init(e);
+                CachedImageRenderer.Init();
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
